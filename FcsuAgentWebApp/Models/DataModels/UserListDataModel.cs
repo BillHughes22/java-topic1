694ee0a76e2c9cf880207e55a4a1287801858b74
pinpointnaga <pinpointnaga@gmail.com>
2021-05-13 15:29:02 -0400

    added updated date

--- FcsuAgentWebApp/Models/DataModels/UserListDataModel.cs
+++ FcsuAgentWebApp/Models/DataModels/UserListDataModel.cs
@@ -17,14 +17,18 @@ namespace FcsuAgentWebApp.Models
             FirstName = string.Empty;//2
             MemberNumber = string.Empty;//3 
             AgentNumber = string.Empty;//3 added for userlist
+            UpdatedDate = null;
         }
         public Guid UserPk { get; set; }
         public string UserName { get; set; }
+        public string Comments { get; set; }
         public string Email { get; set; }
         public bool IsDisabled { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }//2
         public string MemberNumber { get; set; }//3 
         public string AgentNumber { get; set; }//4 added for userlist
+
+        public DateTime? UpdatedDate { get; set; }
     }
 }
